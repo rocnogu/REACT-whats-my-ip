fd85d6b2b0d903740907e0c7f670df0f393ec358
rocnogu <rocnogu@abv.bg>
2021-07-20 12:29:48 +0300

    comit four

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import "./index.css";
 import { useState, useEffect } from "react";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import jsonData from "./Data.json";
-import Card from "react-bootstrap/Card";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./Header";
 import Footer from "./Footer";
 import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import L from "leaflet";
+import Bootsrap from "./Bootsrap";
 
 ///////////////
 export default function App() {
@@ -31,172 +27,14 @@ export default function App() {
     }
   }, [data]);
 
-  const markerIcon = new L.Icon({
-    iconUrl:
-      "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-icon-2x.png",
-    iconSize: [35, 45],
-    iconAnchor: [17, 45],
-    popupAnchor: [3, -46]
-  });
   // return  return  return  return  return  return
   return (
     <div className="App">
-      <Card className="bg-dark text-white">
-        <Header />
-        <Card.Text>Find your IP and some info about your country</Card.Text>
-        <div>
-          {data && country ? (
-            <>
-              <div className="details">
-                <span>
-                  IP: <br /> {data.ip}
-                </span>
-                <span>
-                  {" "}
-                  Continent:
-                  <br />
-                  {country.region}, {country.subregion}
-                </span>
-                <span>
-                  Country: <br /> {country.name}, {data.location.country},
-                  <img
-                    src={country.flag}
-                    alt="no flag"
-                    style={{
-                      height: 20
-                    }}
-                  />
-                </span>
-                <span>
-                  Region: <br /> {data.location.region}
-                </span>
-                <span>
-                  City: <br /> {data.location.city}
-                </span>
-                <span>
-                  Provider: <br /> {data.isp}
-                </span>
-                <span>
-                  lat: <br /> {data.location.lat}
-                </span>
-                <span>
-                  lng: <br /> {data.location.lng}
-                </span>
-                <span>
-                  Timezone: <br /> {country.timezones}
-                </span>
-              </div>
-              <br />
-              <br />
-              <MapContainer
-                style={{ height: "80vh", width: "100%" }}
-                center={[data.location.lat, data.location.lng]}
-                zoom={13}
-                scrollWheelZoom={true}
-              >
-                <TileLayer
-                  attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                <Marker
-                  position={[data.location.lat, data.location.lng]}
-                  icon={markerIcon}
-                >
-                  <Popup>Your Location</Popup>
-                </Marker>
-              </MapContainer>
-              <br />
-            </>
-          ) : (
-            "loading..."
-          )}
-        </div>
-      </Card>
-      <br />
-      <br />
-      <br />
-      <hr />
-
+      <Header />
       <Container>
-        <h1> Bootsrap Version</h1>
         <div>
           {data && country ? (
-            <>
-              <div>
-                <Row>
-                  <Col xs={1} md={1}>
-                    IP: <br /> {data.ip}
-                  </Col>
-
-                  <Col xs={2} md={2}>
-                    Continent:
-                    <br />
-                    {country.region}, {country.subregion}
-                  </Col>
-
-                  <Col xs={2} md={2}>
-                    Country: <br /> {country.name}, {data.location.country},
-                    <img
-                      src={country.flag}
-                      alt="no flag"
-                      style={{
-                        height: 20
-                      }}
-                    />
-                  </Col>
-
-                  <Col xs={1} md={1}>
-                    {" "}
-                    Region: <br /> {data.location.region}
-                  </Col>
-
-                  <Col xs={1} md={1}>
-                    City: <br /> {data.location.city}
-                  </Col>
-
-                  <Col xs={2} md={2}>
-                    Provider: <br /> {data.isp}
-                  </Col>
-
-                  <Col xs={1} md={1}>
-                    lat: <br /> {data.location.lat}
-                  </Col>
-
-                  <Col xs={1} md={1}>
-                    lng: <br /> {data.location.lng}
-                  </Col>
-
-                  <Col xs={2} md={2}>
-                    {" "}
-                    Timezone: <br /> {country.timezones}
-                  </Col>
-                </Row>
-              </div>
-              <br />
-              <br />
-              <Row>
-                <Col xs={12} md={12}>
-                  <MapContainer
-                    style={{ height: "80vh", width: "100%" }}
-                    center={[data.location.lat, data.location.lng]}
-                    zoom={13}
-                    scrollWheelZoom={true}
-                  >
-                    <TileLayer
-                      attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    />
-                    <Marker
-                      position={[data.location.lat, data.location.lng]}
-                      icon={markerIcon}
-                    >
-                      <Popup>Your Location</Popup>
-                    </Marker>
-                  </MapContainer>
-                </Col>
-              </Row>
-              <br />
-            </>
+            <Bootsrap data={data} country={country} />
           ) : (
             "loading..."
           )}
